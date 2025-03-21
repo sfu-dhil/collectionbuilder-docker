5037a951ceeea41eecc98a03427a3a7c6143ebdc
Andrew Gardener <andrew.e.gardener@gmail.com>
2025-03-21 12:14:30 -0700

    fix remote_csv_import
    when not setting the env variable

--- app/_plugins/remote_csv_import.rb
+++ app/_plugins/remote_csv_import.rb
@@ -6,12 +6,10 @@ module JekyllRemoteCsvImport
     metadata_file_name = ENV['METADATA_FILE_NAME'] || 'metadata.csv'
 
     # check if metadata file url env variable exists
-    unless ENV['METADATA_FILE_URL']
-      return
-    end
-
-    File.open("_data/#{metadata_file_name}", 'wb') do |file|
-      file << URI.open(metadata_file_url).read
+    if metadata_file_url && metadata_file_name
+      File.open("_data/#{metadata_file_name}", 'wb') do |file|
+        file << URI.open(metadata_file_url).read
+      end
     end
   end
 end
